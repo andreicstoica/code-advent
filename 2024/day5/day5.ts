8ece96f3264f923a591e90e049adccb9f55e223a
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-27 10:02:23 -0400

    day 2

--- 2024/day5/day5.ts
+++ 2024/day5/day5.ts
@@ -35,3 +35,47 @@ for (const update of updates) {
 }
 
 // PART 2
+
+let score2 = 0;
+for (const update of updates) {
+  let anySwaps = false;
+  const nums = update.split(",").map(Number);
+  const numSet = new Set(nums);
+
+  // repeat until a full pass with no swaps
+  let swappedInPass;
+  do {
+    swappedInPass = false;
+    for (const rule of rules) {
+      const parts = rule.split("|");
+
+      // making sure i'm only getting the rules wiht 2 parts
+      if (parts.length === 2) {
+        const [a, b] = parts.map(Number);
+
+        // if both nums of rule apply
+        if (numSet.has(a!) && numSet.has(b!)) {
+          const idxA = nums.indexOf(a!);
+          const idxB = nums.indexOf(b!);
+
+          // if incorrect order (a after b)
+          if (idxA > idxB) {
+            nums.splice(idxB, 0, nums[idxA]!);
+            //delete nums[idxA + 1]; <-- delete leaves an empty hole at delete idx
+            nums.splice(idxA + 1, 1);
+            anySwaps = true;
+            swappedInPass = true;
+          }
+        }
+      }
+    }
+  } while (swappedInPass);
+
+  // need to fix otherwise
+  if (anySwaps) {
+    const middle = nums[Math.floor(nums.length / 2)]!; // only get middle at the end
+    score2 += middle;
+  }
+}
+
+console.log(score2);
